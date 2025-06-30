58f7e0af74013589ce21b14a45bd6f9ecb4e7545
hnyot <hnyotmyetwunn97@gmail.com>
2025-06-30 22:49:56 +0700

    Training and hyperparameter tuning in progress

---
+++ train_model.py
@@ -0,0 +1,76 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
+import os
+
+
+data_dir = 'data'
+img_height, img_width = 128, 128
+batch_size = 32
+epochs = 20
+
+
+train_datagen = ImageDataGenerator(
+    validation_split=0.2,
+    rescale=1./255,
+    rotation_range=15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+train_generator = train_datagen.flow_from_directory(
+    data_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical',
+    subset='training'
+)
+
+val_generator = train_datagen.flow_from_directory(
+    data_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical',
+    subset='validation'
+)
+
+# MODEL
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)),
+    MaxPooling2D(2, 2),
+
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(train_generator.num_classes, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+# Train
+early_stop = EarlyStopping(monitor='val_loss', patience=3)
+
+history = model.fit(
+    train_generator,
+    validation_data=val_generator,
+    epochs=epochs,
+    callbacks=[early_stop]
+)
+
+# ==== SAVE ====
+model.save('hand_gesture_cnn_model.h5')
+print("✅ Trained Model is saved as hand_gesture_cnn_model.h5")
